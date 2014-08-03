cd37b1aa92e4887df9c69cbcebf4cdc6aa644664
SWATI KIRVE <kirveswati@gmail.com>
2014-08-03 15:41:43 +0100

    Added Chatrooms in tab bar, updated rest tab bar icons

---
+++ TChat/Classes/Controllers/TabBar/Chatrooms/TCChatroomsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCChatroomsViewController.h
+//  TChat
+//
+//  Created by SWATI KIRVE on 03/08/2014.
+//  Copyright (c) 2014 Tusk Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCChatroomsViewController : UIViewController
+
+@end
