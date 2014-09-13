d2ee5bb9bf4edd98ef9bd57afef313813d48729a
Donald King <donaldking247@gmail.com>
2014-09-13 15:07:16 +0100

    => Keychain reset when app lunches... Not good!

--- TChat/Classes/Constants/TCConstants.m
+++ TChat/Classes/Constants/TCConstants.m
@@ -11,8 +11,8 @@
 @implementation TCConstants
 
 NSString *URL_SCHEME = @"https://";
-NSString *XMPP_UAT_HOST = @"dev.yookoschat.com";
-NSString *XMPP_CONFERENCE_UAT_HOST = @"conference.dev.yookoschat.com";
+NSString *XMPP_UAT_HOST = @"uat.yookoschat.com";
+NSString *XMPP_CONFERENCE_UAT_HOST = @"conference.uat.yookoschat.com";
 NSString *XMPP_LIVE_HOST = @"yookoschat.com";
 NSString *PROXY_SERVICE = @"service/proxy/proxy.yookos.php?";
 
